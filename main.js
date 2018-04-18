96af7b4aca3b1e31f60b509ac3bf269dc023f4ad
liumin1128 <970568830@qq.com>
2018-04-18 21:27:09 +0800

    gx1

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const path = require('path');
 const url = require('url');
 const pkg = require('./package.json'); // 引用package.json
 
+const logo = path.join(__dirname, 'public/WeFlow.png');
+
 // 保持一个对于 window 对象的全局引用，如果你不这样做，
 // 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
 let win;
@@ -10,10 +12,13 @@ let win;
 function createWindow() {
   // 创建浏览器窗口。
   win = new BrowserWindow({
-    width: 300,
+    width: 900,
     height: 600,
     // 去掉菜单栏
-    frame: false,
+    // frame: false,
+    titleBarStyle: 'hidden-inset',
+    // hasShadow: false,
+    icon: logo,
   });
 
   // 然后加载应用的 index.html。
@@ -38,7 +43,7 @@ function createWindow() {
   // }
 
   // 打开开发者工具。
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools();
 
   BrowserWindow.addDevToolsExtension('/Users/liumin/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/3.2.1_0');
   BrowserWindow.addDevToolsExtension('/Users/liumin/Library/Application Support/Google/Chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.15.2_0');
--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
 import Button from 'material-ui/Button';
-
+import pkg from '../package.json';
 import './App.css';
+
+const electron = window.require('electron');
+const { remote, shell } = electron;
+const { dialog, BrowserWindow } = remote;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <br />
-        <Button>start</Button>
+        <Button onClick={() => {
+          console.log('pkg');
+          const iii = 'ls ./'.split(' ');
+          const [cmd, ...options] = iii;
+
+          RunCmd(cmd, options, (result) => {
+            console.log(result);
+          });
+          // const sss = new BrowserWindow({
+          //   x: 50,
+          //   y: 50,
+          // });
+          // new Notification('提示', {
+          //   body: '打包成功',
+          // });
+          // shell.showItemInFolder('/Users/liumin/Desktop/Electron/WeFlow');
+          // shell.openExternal('');
+          // shell.openExternal('https://github.com');
+          // dialog.showMessageBox({ title: '8888', message: '6666' });
+          // console.log(dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] }));
+        }}
+        >start</Button>
       </div>
     );
   }
+}
+
+
+function RunCmd(cmd, args, cb) {
+  const { spawn } = window.require('child_process');
+  const child = spawn(cmd, args);
+  let result = '';
+  child.stdout.on('data', (data) => {
+    result += data.toString();
+  });
+  child.stdout.on('end', () => {
+    cb(result);
+  });
+  child.stderr.on('data', (data) => {
+    console.log(`Error: \n${data}`);
+  });
+  child.on('exit', (code, signal) => {
+    console.log(`Exit: ${code}`);
+  });
 }
 
 export default App;
