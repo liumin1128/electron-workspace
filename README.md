0c11d17c7eb9106743815f551bdfd0c5a119d528
liumin1128 <970568830@qq.com>
2018-04-18 16:12:49 +0800

    🎃

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+yarn add next@latest react@latest react-dom@latest prop-types@latest material-ui@next @material-ui/icons
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button>hello world</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
