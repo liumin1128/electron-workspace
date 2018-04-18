541f442329bc628c63024434f1255090ecdca716
liumin1128 <970568830@qq.com>
2018-04-18 22:01:26 +0800

    初步实现基本概念

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
 import React, { Component } from 'react';
 import Button from 'material-ui/Button';
+import Dropzone from 'react-dropzone';
 import pkg from '../package.json';
 import './App.css';
 
 const electron = window.require('electron');
+const fs = window.require('fs');
+
 const { remote, shell } = electron;
 const { dialog, BrowserWindow } = remote;
 
 class App extends Component {
+  state = {
+    list: [],
+  }
+  addProject(obj) {
+    const { list } = this.state;
+    this.setState({
+      list: [...list, obj],
+    });
+  }
   render() {
+    const { list } = this.state;
     return (
       <div className="App">
         <br />
+
+        {
+          list.map(i => (<div>
+            <h1>{i.name}</h1>
+            <h2>{i.path}</h2>
+            <ul>
+              {Object.keys(i.scripts).map(key => (<li>
+                {key}: {i.scripts[key]}
+              </li>))}
+            </ul>
+          </div>))
+        }
+
+        <Dropzone onDrop={(files) => {
+          console.log(files);
+          files.map(({ path, name }) => {
+            console.log(path);
+            const stat = fs.statSync(path);
+            console.log('stat');
+            console.log(stat);
+            if (stat.isDirectory()) {
+              const hasPkg = fs.existsSync(`${path}/package.json`);
+              if (hasPkg) {
+                const itemPkg = window.require(`${path}/package.json`);
+                console.log('itemPkg');
+                console.log(itemPkg);
+                this.addProject({
+                  path,
+                  name: itemPkg.name || name,
+                  scripts: itemPkg.scripts,
+                });
+              } else {
+                const tip = new Notification('提示', {
+                  body: '无法检测到package.json',
+                });
+              }
+            }
+          });
+        }}
+        >
+          <p>Try dropping some files here, or click to select files to upload.</p>
+        </Dropzone>
+
         <Button onClick={() => {
           console.log('pkg');
           const iii = 'ls ./'.split(' ');
